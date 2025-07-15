f8da87882f9964969a71046aa46d53d71e95ae37
jose-guerrerog <joseguerrerog880@gmail.com>
2025-07-16 05:41:08 +1000

    clean up

--- src/api/main.py
+++ src/api/main.py
@@ -1,33 +1,18 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import torch
-import torch.nn as nn
+from src.model import CryptoClassifier
+import os
 
-# Define input schema
 class FeatureInput(BaseModel):
-    features: list[float]  # Expecting 5 input features
+    features: list[float]  
 
-# Define model
-class CryptoClassifier(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(5, 64),
-            nn.ReLU(),
-            nn.Linear(64, 32),
-            nn.ReLU(),
-            nn.Linear(32, 2)
-        )
-
-    def forward(self, x):
-        return self.net(x)
-
-# Load the trained model
 model = CryptoClassifier()
-model.load_state_dict(torch.load("./src/model.pth", map_location=torch.device("cpu")))
+MODEL_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..", "models", "crypto_classifier.pth"))
+model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device("cpu")))
 model.eval()
 
-# Create FastAPI app
+# FastAPI app
 app = FastAPI()
 
 @app.post("/predict")
---
+++ src/model.py
@@ -0,0 +1,15 @@
+import torch.nn as nn
+
+class CryptoClassifier(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(5, 64),
+            nn.ReLU(),
+            nn.Linear(64, 32),
+            nn.ReLU(),
+            nn.Linear(32, 2)
+        )
+
+    def forward(self, x):
+        return self.net(x)
